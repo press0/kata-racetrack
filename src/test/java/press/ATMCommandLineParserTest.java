2f9c73f07884a7ebf7bc2de163a399c2e79b23d0
press0@gmail.com <press0@gmail.com>
2022-12-27 14:44:16 -0600

    dep bot sec alerts

--- src/test/java/press/ATMCommandLineParserTest.java
+++ src/test/java/press/ATMCommandLineParserTest.java
@@ -1,36 +1,37 @@
 package press;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ATMCommandLineParserTest {
 
-	ATMCommandLineParser parser;
+    ATMCommandLineParser parser = new ATMCommandLineParser();
 
-	@Before
-	public void setUp() throws Exception {
-		parser = new ATMCommandLineParser();
-	}
+    //TODO
+    @BeforeEach
+    public void setUp() throws Exception {
+//        parser = new ATMCommandLineParser();
+    }
 
-	@Test
-	public void testParse_SetWinner() throws Exception {
-		ATMCommand command;
-		command = parser.parse("W 1");
-		assertTrue(command.equals(ATMCommand.SETWINNER));
-		assertTrue(parser.getMessage().equals("Set Winner to: 1"));
-		assertTrue(parser.getWinner() == 1);
-	}
+    @Test
+    public void testParse_SetWinner() throws Exception {
+        ATMCommand command;
+        command = parser.parse("W 1");
+        assertTrue(command.equals(ATMCommand.SETWINNER));
+        assertTrue(parser.getMessage().equals("Set Winner to: 1"));
+        assertTrue(parser.getWinner() == 1);
+    }
 
-	@Test
-	public void testParse_SetWager() throws Exception {
-		ATMCommand command;
-		command = parser.parse("1 2");
-		assertTrue(command.equals(press.ATMCommand.WAGER));
-		assertTrue(parser.getMessage().equals(""));
-		assertTrue(parser.getHorseNumber() == 1);
-		assertTrue(parser.getAmountOfWager() == 2);
-	}
+    @Test
+    public void testParse_SetWager() throws Exception {
+        ATMCommand command;
+        command = parser.parse("1 2");
+        assertTrue(command.equals(press.ATMCommand.WAGER));
+        assertTrue(parser.getMessage().equals(""));
+        assertTrue(parser.getHorseNumber() == 1);
+        assertTrue(parser.getAmountOfWager() == 2);
+    }
 
 }
--- src/test/java/press/HorseRaceImplTest.java
+++ src/test/java/press/HorseRaceImplTest.java
@@ -1,28 +1,31 @@
 package press;
 
-import org.junit.Before;
-import org.junit.Test;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HorseRaceImplTest {
 
-	HorseRace horseRace;
+    HorseRace horseRace = new HorseRaceImpl();
 
-	@Before
-	public void setUp() throws Exception {
-		horseRace = new HorseRaceImpl();
-	}
+    //TODO:
+    @BeforeEach
+    public void setUp() throws Exception {
+//        horseRace = new HorseRaceImpl();
+    }
 
-	@Test
-	public void testSetWinner() throws Exception {
-		assertTrue(horseRace.getHorseRaceEntryByNumber(1).getValue().equals(HorseRaceResult.WIN));
-		horseRace.setWinner(2);
-		assertFalse(horseRace.getHorseRaceEntryByNumber(1).getValue().equals(HorseRaceResult.WIN));
-	}
+    @Test
+    public void testSetWinner() throws Exception {
+        assertTrue(horseRace.getHorseRaceEntryByNumber(1).getValue().equals(HorseRaceResult.WIN));
+        horseRace.setWinner(2);
+        assertFalse(horseRace.getHorseRaceEntryByNumber(1).getValue().equals(HorseRaceResult.WIN));
+    }
 
-	@Test
-	public void testGetHorseRaceEntryByNumber() throws Exception {
-		assertTrue(horseRace.getHorseRaceEntryByNumber(7).getKey().getNumber() == 7);
-	}
+    @Test
+    public void testGetHorseRaceEntryByNumber() throws Exception {
+        assertTrue(horseRace.getHorseRaceEntryByNumber(7).getKey().getNumber() == 7);
+    }
 }
